62d70288cd01fbac6bc9370f4987227c7b9a23e6
essevpi <essevpi@gmail.com>
2023-04-06 17:37:20 +0200

    add projects section, minor style changes

--- src/sections/projects/projectsData.js
+++ src/sections/projects/projectsData.js
@@ -3,28 +3,10 @@ import WeatherioPreview from '../../../public/images/weatherio_preview.png'
 const projectsData = [
   {
     title: 'Weatherio',
-    desc: `A simple weather app built with NextJS and Redux Toolkit for state management`,
-    href: 'https://my-weatherio.vercel.app/',
-    img: WeatherioPreview,
-  },/* 
-  {
-    title: 'Weatherio2',
-    desc: `A simple weather app built with NextJS and Redux Toolkit for state management`,
+    desc: 'A simple weather app built with NextJS and Redux Toolkit for state management',
     href: 'https://my-weatherio.vercel.app/',
     img: WeatherioPreview,
   },
-  {
-    title: 'Weatherio3',
-    desc: `A simple weather app built with NextJS and Redux Toolkit for state management`,
-    href: 'https://my-weatherio.vercel.app/',
-    img: WeatherioPreview,
-  },
-  {
-    title: 'Weatherio4',
-    desc: `A simple weather app built with NextJS and Redux Toolkit for state management`,
-    href: 'https://my-weatherio.vercel.app/',
-    img: WeatherioPreview,
-  }, */
 ];
 
 export default projectsData;
